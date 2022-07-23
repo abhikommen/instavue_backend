fff60f8fc8d8529e9a2b87a2c671bf15a2fcba37
Abhishek <abhishek.dhyani@inmobi.com>
2022-07-23 22:21:36 +0530

    Json changes

--- dao/storiesdao.js
+++ dao/storiesdao.js
@@ -14,7 +14,17 @@ async function getProfile(userName) {
         "method": "GET"
     });
     try{
-        var json = await result.json()
+        var json = {}
+        var jsonResult = await result.json()
+        json.id = jsonResult.result.id
+        json.username = jsonResult.result.username
+        json.is_private = jsonResult.result.is_private
+        json.pfp = jsonResult.result.profile_pic_url
+        json.bio = jsonResult.result.biography
+        json.name = jsonResult.result.full_name
+        json.followers = jsonResult.result.edge_followed_by.count
+        json.following = jsonResult.result.edge_follow.count
+
         return json
     } catch(error){
         return {
