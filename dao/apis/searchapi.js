7fe39de7a9303268127ec0453df278120bc4c2da
Abhishek <abhishek.dhyani@inmobi.com>
2022-08-28 02:01:13 +0530

    Changes

--- dao/apis/searchapi.js
+++ dao/apis/searchapi.js
@@ -13,17 +13,23 @@ export async function SearchProfile(userName, headers) {
             throw new ErrorModel(440, "Cookie or appid not present in the header request")
         }
 
-        let result = await fetch(`https://www.instagram.com/web/search/topsearch/?query=${userName}`, {
+        let result = await fetch(`https://i.instagram.com/api/v1/web/search/topsearch/?context=blended&query=${userName}`, {
             "headers": {
                 "accept": "*/*",
                 "accept-language": "en-GB,en;q=0.9,en-US;q=0.8",
+                "sec-ch-ua": "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Microsoft Edge\";v=\"104\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"macOS\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-site",
+                "x-ig-app-id": headers.appid,
+                "cookie": headers.cookie,
+                "Referer": "https://www.instagram.com/",
+                "Referrer-Policy": "strict-origin-when-cross-origin"
             },
             "body": null,
-            "method": "GET",
-            "mode": "cors",
-            "x-ig-app-id": headers.appid,
-            "cookie": headers.cookie,
-            "credentials": "include"
+            "method": "GET"
         });
 
         var code = result.status
