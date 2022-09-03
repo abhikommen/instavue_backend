d2b9096aabc2db2461f59419b0ce5ab3bd04a0ea
Abhishek <abhishek.dhyani@inmobi.com>
2022-09-03 17:08:07 +0530

    changes

--- dao/apis/highlightapi.js
+++ dao/apis/highlightapi.js
@@ -3,41 +3,43 @@ import ProfileEntity from '../model/profilemodel.js'
 import ErrorModel from '../model/error.js'
 import ResultResponse from '../model/resultresponse.js'
 import HighlightModel from '../model/hightlight.js'
+import { LoginApi } from './loginapi.js'
 
 import fetch from 'node-fetch';
 
 
 export async function GetHighlight(userId, headers) {
-    try {
-  
-      if (headers.query_hash === undefined) {
-        throw new ErrorModel(401, "Query Hash is missing in the header request")
-      }
-  
-      let result = await fetch(`https://www.instagram.com/graphql/query/?query_hash=${headers.query_hash}&variables={"user_id":${userId},"include_chaining":true,"include_reel":true,"include_suggested_users":false,"include_logged_out_extras":false,"include_highlight_reels":true,"include_live_status":true}`)
-      let code = result.status
-      return new ResultResponse(code, await result.text())
-      if (code === 200) {
-        var rawJson = await CheckSession(result)
-        const highlightsArray = []
+  try {
 
-        rawJson.data.user.edge_highlight_reels.edges.forEach((edge) => {
-           let highlight = new HighlightModel(
-                edge.node.id,
-                edge.node.title,
-                edge.node.cover_media_cropped_thumbnail.url
-           )
-           highlightsArray.push(highlight)
-        })
-        
-        return new ResultResponse(code, highlightsArray)
-      } else {
-        return new ErrorModel(440, "Session Expire!!")
-      }
-    } catch (error) {
-      console.log(error)
-      return error
+    if (headers.query_hash === undefined) {
+      throw new ErrorModel(401, "Query Hash is missing in the header request")
     }
-  
+
+    var loginApiResult = await LoginApi(headers)
+    console.log(loginApiResult)
+    let result = await fetch(`https://www.instagram.com/graphql/query/?query_hash=${loginApiResult.result.query_hash}&variables={"user_id":${userId},"include_chaining":true,"include_reel":true,"include_suggested_users":false,"include_logged_out_extras":false,"include_highlight_reels":true,"include_live_status":true}`)
+    let code = result.status
+    return new ResultResponse(code, await result.text())
+    if (code === 200) {
+      var rawJson = await CheckSession(result)
+      const highlightsArray = []
+
+      rawJson.data.user.edge_highlight_reels.edges.forEach((edge) => {
+        let highlight = new HighlightModel(
+          edge.node.id,
+          edge.node.title,
+          edge.node.cover_media_cropped_thumbnail.url
+        )
+        highlightsArray.push(highlight)
+      })
+
+      return new ResultResponse(code, highlightsArray)
+    } else {
+      return new ErrorModel(440, "Session Expire!!")
+    }
+  } catch (error) {
+    console.log(error)
+    return error
   }
-  
+
+}
