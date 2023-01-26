843d3d34c74e6456146645891432fb0fa53d7ede
Abhishek Dhyani <abhishek.dhyani@IM5430-X0.local>
2023-01-26 15:37:45 +0530

    Homepage public url added

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import router from './routes/routes.js'
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', router)
 
 app.listen(process.env.PORT || 5555, () => {
